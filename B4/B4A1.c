bcfe7597d7945d70631bb6c78d002ecb094c2683
Andreas Diesendorf <andreas_d@hotmail.de>
2019-11-02 14:36:16 +0100

    Added .gitignore to ignore executables. Added finished B4A1

--- B4/B4A1.c
+++ B4/B4A1.c
@@ -1,5 +1,17 @@
-#include ....
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
+/**
+ * Gruppe 7
+ * Andreas Diesendorf
+ * Artur Siubiak
+ * Jan Wolfram
+ * 
+ * Kompiliert mit:
+ * clang -Wall -Wextra -pedantic -std=c99 -g B4A1.c -o B4A1
+**/
 
 typedef struct {
   char* vorname;
@@ -14,6 +26,7 @@ void schreibe_eintrag(
       char* nachname,
       char* telefonnummer)
 {
+  telefonbucheintrag *eintrag = eintrag_ptr;
   // Anmerkung: p->x ist eine Abkuerzung von (*(p)).x
   eintrag->vorname       = malloc(strlen(vorname)+1);
   eintrag->nachname      = malloc(strlen(nachname)+1);
@@ -27,12 +40,37 @@ void drucke_telefonbuch(
       telefonbucheintrag* telefonbuch,
       int32_t anz_eintraege)
 {
+  telefonbucheintrag *eintrag = telefonbuch;
   // Anmerkung: p->x ist eine Abkuerzung von (*(p)).x
   while (anz_eintraege > 0) {
     printf("%s, %s: %s\n", eintrag->vorname, eintrag->nachname, eintrag->telefonnummer);
     eintrag++;
     --anz_eintraege;
   }
+}
+
+void loesche_telefonbuch(telefonbucheintrag* telefonbuch, int32_t telefonbucheintraege) {
+  telefonbucheintrag* eintrag;
+  for (int i = 0; i < telefonbucheintraege; i++) {
+    eintrag = telefonbuch+i;
+    free(eintrag->telefonnummer);
+    free(eintrag->nachname);
+    free(eintrag->vorname);
+  }
+}
+
+int telefonbucheintrag_comp(const void* comp1, const void* comp2) {
+  telefonbucheintrag eintrag1 = *((telefonbucheintrag*) comp1);
+  telefonbucheintrag eintrag2 = *((telefonbucheintrag*) comp2);
+  
+  int nachname_comp = strcmp(eintrag1.nachname, eintrag2.nachname);
+  
+  if (nachname_comp != 0) return nachname_comp;
+  else {
+    int vorname_comp = strcmp(eintrag1.vorname, eintrag2.vorname);
+    if (vorname_comp != 0) return vorname_comp;
+  }
+  return 0;
 }
 
 int main()
@@ -49,7 +87,7 @@ int main()
   telefonbucheintraege = 5;
   
   drucke_telefonbuch(telefonbuch, telefonbucheintraege);
-  qsort(telefonbuch, ???);
+  qsort(telefonbuch, telefonbucheintraege, sizeof(telefonbucheintrag), &telefonbucheintrag_comp);
   drucke_telefonbuch(telefonbuch, telefonbucheintraege);
   
   loesche_telefonbuch(telefonbuch, telefonbucheintraege); // Damit keine Katzenbabies sterben.
